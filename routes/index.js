2a8cb6c1ba75fb41c5f6f1618b92773763b00fde
Tobenna <tvokafor@gmail.com>
2023-08-01 21:23:08 +0100

    Updated files

--- routes/index.js
+++ routes/index.js
@@ -3,5 +3,5 @@ const express = require("express");
 const notesRouter = require("./notes");
 
 const app = express();
-app.use("/notess", notesRouter);
+app.use("/notes", notesRouter);
 module.exports = app;
--- server.js
+++ server.js
@@ -12,10 +12,10 @@ app.use("/api", api);
 app.use(express.static("public"));
 
 app.get("/", (req, res) =>
-  res.sendFile(path.join(_dirname, "public/index.html"))
+  res.sendFile(path.join(__dirname, "public/index.html"))
 );
 app.get("/notes", (req, res) =>
-  res.sendFile(path.join(_dirname, "public/notes.html"))
+  res.sendFile(path.join(__dirname, "public/notes.html"))
 );
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
